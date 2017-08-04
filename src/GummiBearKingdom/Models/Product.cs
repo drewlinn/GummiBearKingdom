7925136115cffb8332d75775b51a989faf52c38a
Andrew <expandrew@gmail.com>
2017-08-04 12:13:34 -0700

    build out most of our required file structure.

---
+++ src/GummiBearKingdom/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public float cost { get; set; }
+        public string origin { get; set; }
+    }
+}
